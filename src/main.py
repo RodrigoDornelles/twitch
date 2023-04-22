4c83a3f24c76873323e6ec7ff39d4ecfe6e60196
RodrigoDornelles <rodrigo@dornelles.me>
2023-04-22 16:33:26 -0300

    feat: twitch tts

--- src/main.py
+++ src/main.py
@@ -1,14 +1,41 @@
+from twitchio.ext import commands
+from dotenv import load_dotenv
+from os import environ
 import subprocess
+import random
 import os
-import time
 
-cmd_arr = (
- 'espeak', '-vbrazil-mbrola-4', 'amigo voce é um amigo, obrigado amigo.',
- '-p', '300', '-s', '100'
-)
+load_dotenv()
 
-subprocess.Popen(cmd_arr)
+class Bot(commands.Bot):
 
-print('foo')
-time.sleep(2)
-print('bar')
+    def __init__(self):
+        super().__init__(token=environ['TWITCH_TOKEN'],
+            prefix=environ['TWITCH_PREFIX'],
+            initial_channels=[environ['TWITCH_CHANNEL']]
+        )
+
+    async def event_ready(self):
+        print(f'Logged in as | {self.nick}')
+        print(f'User id is | {self.user_id}')
+
+    async def event_message(self, message):
+        if message.echo:
+            return
+
+        random.seed(message.author.color)
+
+        cmd_arr = (
+            'espeak', '-vbrazil-mbrola-4', message.content,
+            '-p', str(random.randint(1, 200)), '-s', str(random.randint(90, 140))
+        )
+        subprocess.Popen(cmd_arr)
+
+        await self.handle_commands(message)
+
+    @commands.command()
+    async def hello(self, ctx: commands.Context):
+        await ctx.send(f'Hello {ctx.author.name}!')
+
+bot = Bot()
+bot.run()
